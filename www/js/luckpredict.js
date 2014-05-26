4a9b671b161c0087d66e796889808031909ff09b
venkatesh khandare <venkatesh.khandare@gmail.com>
2014-05-27 01:42:21 +0800

    first upload
    This is the first upoad

---
+++ www/js/luckpredict.js
@@ -0,0 +1,28 @@
+var PREDICT;
+var PRIORITY = [];
+
+function predict(temp_predict){
+    //alert(predict); /*should display predict */
+    //alert(PREDICT); /*should display undefined */
+    PREDICT=temp_predict;
+    //alert(PREDICT);  /*should display predict as a global variable value*/  
+ }
+function priority(){
+   //alert(PREDICT);
+    var predict_ids=["finance","love","education","work","travel"];
+    //alert("test");
+	var temppriority = []; /* initialised to make PRIORITY to have null at inital stages */
+	PRIORITY = temppriority; /* to make PRIORITY NULL and then store ids and values to it */
+    for(var i=0;i<predict_ids.length;i++){        
+        var e = document.getElementById(predict_ids[i]);       
+        var priority_value = e.options[e.selectedIndex].value;         
+		PRIORITY.push({id:predict_ids[i],value:priority_value});		
+	}
+		for(var i=0;i<PRIORITY.length;i++){
+			alert("ID:- "  + PRIORITY[i].id + " Value:- "  + PRIORITY[i].value);
+			}
+    alert(PREDICT);
+    // To check the values if entered correctly or not.
+   
+    
+}
