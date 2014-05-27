c5b4e009a73a9fa24862d4dd6b395e721c0ff7ed
venkatesh khandare <venkatesh.khandare@gmail.com>
2014-05-27 22:55:14 +0800

    border to image
    Image css file is added webkit border and shadow

--- www/js/luckpredict.js
+++ www/js/luckpredict.js
@@ -18,11 +18,23 @@ function priority(){
         var priority_value = e.options[e.selectedIndex].value;         
 		PRIORITY.push({id:predict_ids[i],value:priority_value});		
 	}
-		for(var i=0;i<PRIORITY.length;i++){
+	/*	for(var i=0;i<PRIORITY.length;i++){
 			alert("ID:- "  + PRIORITY[i].id + " Value:- "  + PRIORITY[i].value);
 			}
     alert(PREDICT);
     // To check the values if entered correctly or not.
-   
+   */
     
+}
+
+
+function update(id){	
+	alert(id);
+	var yourbriefcase = document.getElementById('yourbriefcase').src.indexOf('yourbriefcase.png');
+	alert(yourbriefcase);
+		 if(document.getElementById("yourbriefcase").src.indexOf('yourbriefcase.png')>=0) {
+			 	document.getElementById("yourbriefcase").src = document.getElementById(id).src;
+		 }
+
+	 document.getElementById(id).src="images/empty.png";
 }
